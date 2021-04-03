a95cb4e37a67619725d5e0118742d4f118ad7178
MichalRiskeDeveloper <michalriskedeveloper@gmail.com>
2021-04-03 04:36:53 +0200

    Initial Commit

---
+++ IconPalace/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  IconPalace
+//
+//  Created by Michał Riske on 03/04/2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
